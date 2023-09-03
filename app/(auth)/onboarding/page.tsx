b43d0f6d950c8639753c8bb30f061e455df3d579
Kumar <kumar.deepam@gmail.com>
2023-09-03 16:37:14 +0530

    setup shadcn and zod and get the basic setup in place to start working with forms

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,43 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs';
+
 async function Page() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+
+  // const userInfo = await fetchUser(user.id);
+  // if (userInfo?.onboarded) redirect('/');
+
+  const currentuser = await currentUser();
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?.objectId,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user.firstName || '',
+    bio: userInfo?.bio || '',
+    image: userInfo?.image || user.imageUrl,
+  };
+
+  // const userData = {
+  //   id: user?.id,
+  //   objectId: '',
+  //   username: user?.username || '',
+  //   name: user.firstName || '',
+  //   bio: '',
+  //   image: user.imageUrl,
+  // };
+
   return (
-    <main>
-      <h1>Onboarding page</h1>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding page</h1>
+      <p className="mt-3 text-base-regular  text-light-2">
+        Complete your profile now to start using Threads!
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <main>
+          <main className="flex flex-row">
             <Topbar />
             <LeftSidebar />
             <section className="main-container">
